7419fa9d6e6f913224922ea6ffabd26712f94f5f
Daniel Antunes <daniel.antunes.nutri@gmail.com>
2023-07-04 18:01:08 -0300

    adicionado funcionalidade para criação de arquivo e texto personalizado

--- conversão.py
+++ conversão.py
@@ -1,9 +1,24 @@
 from gtts import gTTS
 import playsound
 
-arquivo = open('texto.txt', 'r', encoding='UTF-8').read()
-txt_para_audio = gTTS(arquivo, lang='pt-br')
-txt_para_audio.save('poema.mp3')
+# Criando arquivo de texto
 
+nome_arquivo = str(input('Digite um nome para o arquivo de texto (sem o .txt): ')) + '.txt'
+texto = str(input('Digite o texto que será escrito no arquivo: '))
+arquivo = open(f'{nome_arquivo}', 'w', encoding='UTF-8')
+# Escrevendo o texto no arquivo criado.
+arquivo.write(texto)
+# Fechando o arquivo
+arquivo.close()
+
+
+# Lendo arquivo de texto
+arquivo = open(nome_arquivo, 'r', encoding='UTF-8').read()
+# Transformando em mp3 e salvando
+txt_para_audio = gTTS(arquivo, lang='pt-br')
+arquivo_mp3 = nome_arquivo.replace('.txt', '.mp3')
+txt_para_audio.save(arquivo_mp3)
+
+# Reproduzindo o mp3 (do texto que foi transformado)
 # Deve-se ter cuidado, pois se o diretório mudar, o arquivo não reproduzirá.
-playsound.playsound(r'C:\Users\Daniel\Documents\Projetos Python\txt_para_mp3\poema.mp3')
+playsound.playsound(fr'C:\Users\Daniel\Documents\Projetos Python\txt_para_mp3\{arquivo_mp3}')
